4c3256ba09179cc29e7910f2c739083a70956c8b
tyang <tyang@indeed.com>
2020-10-02 21:53:18 -0700

    remove unused code

--- src/main/java/nomadyt/aws/step/functions/activity/framework/config/ActivityHeartbeatExecutorConfig.java
+++ src/main/java/nomadyt/aws/step/functions/activity/framework/config/ActivityHeartbeatExecutorConfig.java
@@ -1,17 +1,11 @@
 package nomadyt.aws.step.functions.activity.framework.config;
 
-import com.amazonaws.ClientConfiguration;
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.stepfunctions.AWSStepFunctions;
-import com.amazonaws.services.stepfunctions.AWSStepFunctionsClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class ActivityHeartbeatExecutorConfig {
@@ -21,20 +15,6 @@ public class ActivityHeartbeatExecutorConfig {
     @Bean
     public ScheduledExecutorService activityHeartbeatExecutorService() {
         return Executors.newScheduledThreadPool(activityHeartbeatWorkerPoolSize);
-    }
-
-    @Bean
-    public AWSStepFunctions awsStepFunctionsClient(
-            final AWSCredentialsProvider awsCredentialsProvider
-    ) {
-        ClientConfiguration clientConfiguration = new ClientConfiguration();
-        clientConfiguration.setSocketTimeout((int) TimeUnit.SECONDS.toMillis(70));
-
-        return AWSStepFunctionsClientBuilder.standard()
-                .withRegion(Regions.US_EAST_1)
-                .withCredentials(awsCredentialsProvider)
-                .withClientConfiguration(clientConfiguration)
-                .build();
     }
 }
 
